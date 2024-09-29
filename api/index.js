27f881bf6f9667430389bee6bc8e508f3c537190
deepak <sidhudeep9292@gmail.com>
2024-09-29 15:51:38 +0100

    connected to the database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoUri = process.env.MONGODB_URI;
+if (!mongoUri) {
+    throw new Error('MONGODB_URI is not defined in the environment variables');
+}
+
+mongoose.connect(mongoUri)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB', err));
 
 const app = express();
 
-
-
-
-app.listen(3002, () => {
-    console.log('Server is running on port 3002 !!!');
+app.listen(3001, () => {
+    console.log('Server is running on port 3000 !!!');
 });
 
 export default app;
