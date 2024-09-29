3efef72645c1fabcc2483474f341c782577f4f6c
deepak <sidhudeep9292@gmail.com>
2024-09-29 16:53:34 +0100

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from './routes/user.route.js';
+const app = express();
 dotenv.config();
+
+
+
+app.use("/api/user", userRouter);
+
+
+
+
+
 
 const mongoUri = process.env.MONGODB_URI;
 if (!mongoUri) {
     throw new Error('MONGODB_URI is not defined in the environment variables');
 }
-
 mongoose.connect(mongoUri)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
-
-const app = express();
 
 app.listen(3001, () => {
     console.log('Server is running on port 3000 !!!');
