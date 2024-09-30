ae8cbaf57d0b641e4695e358a5a0b49faf821b30
deepak <sidhudeep9292@gmail.com>
2024-09-30 17:49:31 +0100

    create signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,7 +7,7 @@ export const signup = async (req, res , next) => {
   const newUser = new User({ username, email, password: hashedPassword });
      try {
     await newUser.save();
-    res.status(201).json( newUser, "was created" );
+    res.status(201).json(`${newUser} was created`);
     }
      catch (error) {
     next(error);
