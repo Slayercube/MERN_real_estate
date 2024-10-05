60f2552e5ce652a422d97a9d3a928300d47b7541
deepak <sidhudeep9292@gmail.com>
2024-10-05 15:32:12 +0100

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
 const app = express();
 dotenv.config();
@@ -12,6 +13,8 @@ app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
---
+++ api/routes/listing.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import {createListing} from "../controllers/listing.controller.js";
+import {verifyUser} from "../utiles/verifyUser.js";
+
+const router = express.Router();
+
+router.post('/create',verifyUser , createListing)
+
+export default router;
