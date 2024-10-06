ab0eb03d9966eb59c5e9bda13fef50aaf36d6d81
deepak <sidhudeep9292@gmail.com>
2024-10-06 19:19:09 +0100

    create user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import {errorHandler} from "../utiles/error.js";
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 
 
@@ -43,3 +44,19 @@ export const deleteUser = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user._id === req.params.id){
+        try {
+            const listings = await Listing.find({ userRef: req.params.id });
+            res.status(200).json(listings);
+        }
+        catch (error) {
+            next(error);
+    }
+    }
+    else {
+        return next(errorHandler(403, "you can only view your listings"));
+    }
+
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {deleteUser, test, updateUser} from '../controllers/user.controller.js';
+import {deleteUser, test, updateUser, getUserListings} from '../controllers/user.controller.js';
 import {verifyUser} from "../utiles/verifyUser.js";
 const router = express.Router();
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/test', test)
 router.post('/update/:id', verifyUser, updateUser)
 router.delete('/delete/:id', verifyUser, deleteUser)
+router.get('/listings/:id', verifyUser, getUserListings)
 
 
 
