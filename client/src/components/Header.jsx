abc71ef5f60140eeb66c0ae000481eaa01501b89
deepak <sidhudeep9292@gmail.com>
2024-10-08 21:36:13 +0100

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-  const { user } = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const urlParams = new URLSearchParams(window.location.search);
+    // urlParams.set("searchTerm", searchTerm);
+    // const searchQuery = urlParams.toString();
+    // navigate(`/search?${searchQuery}`);
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFormUrl = urlParams.get("searchTerm");
+    if (searchTermFormUrl) {
+      setSearchTerm(searchTermFormUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -13,13 +35,19 @@ const Header = () => {
           <h1 className="font-bold text-sm sm-text-xl flex flex-wrap"></h1>
         </Link>
 
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search"
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-500" />
+          <button type="submit" className="text-slate-500">
+            <FaSearch className="text-slate-500" />
+          </button>
         </form>
 
         <ul className="flex gap-4">
@@ -36,9 +64,9 @@ const Header = () => {
           </Link>
 
           <Link to={"/profile"} className="text-slate-700 hover:underline">
-            {user.currentUser ? (
+            {currentUser ? (
               <img
-                src={user.currentUser.avatar}
+                src={currentUser.avatar}
                 alt="profile"
                 className="w-8 h-8 rounded-full object-cover"
               />
