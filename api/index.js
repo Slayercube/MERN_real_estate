a4add783281e0927f53518b18f567756a4d6acd4
deepak <sidhudeep9292@gmail.com>
2024-10-09 21:49:25 +0100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,15 +5,25 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
+const __dirname = path.resolve();
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+}
+);
 
 
 app.use((err, req, res, next) => {
